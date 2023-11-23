92b7ae2e0b5d408b628c386cf5f9f016c6eb67f4
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-23 15:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here!
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Initial list items
+    const initialItems = 10;
+    const myList = document.getElementById("myList");
+
+    for (let i = 1; i <= initialItems; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `List item ${i}`;
+        myList.appendChild(listItem);
+    }
+
+    // Function to add more list items when scrolling to the bottom
+    function addMoreItems() {
+        const totalItems = myList.childElementCount;
+        const scrollPosition = window.scrollY;
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+
+        // Check if the user has scrolled to the bottom
+        if (scrollPosition + windowHeight >= documentHeight - 20) {
+            // Add 2 more list items
+            for (let i = 1; i <= 2; i++) {
+                const newItem = document.createElement("li");
+                newItem.textContent = `List item ${totalItems + i}`;
+                myList.appendChild(newItem);
+            }
+        }
+    }
+
+    // Attach the addMoreItems function to the scroll event
+    window.addEventListener("scroll", addMoreItems);
+});
